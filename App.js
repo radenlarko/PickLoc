a580f33d7b66b49a1fa428e8f5d8fe280e0991c5
radenlarko <radenlarko@gmail.com>
2021-06-27 23:00:10 +0700

    commit pertama

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import Maps from './src/screens/Maps';
+
+const App = () => {
+  return (
+    <Maps />
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
